a7ded217db88e998fbde1c8740f23ceead1c1fec
GE47 <hassan2pac47@gmail.com>
2021-11-16 00:51:51 +0300

    fix(theme): change the Heading properties

--- theme/components/Heading.ts
+++ theme/components/Heading.ts
@@ -1,11 +1,7 @@
 const Heading = {
   variants: {
     title: {
-      textDecoration: "underline",
       fontSize: 20,
-      textUnderlineOffset: 6,
-      textDecorationColor: "#525252",
-      textDecorationThickness: 4,
       marginTop: 3,
       marginBottom: 4,
     },
