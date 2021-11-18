3206a64f243fce6f747efeac5ad0eb4698471bb8
GE47 <hassan2pac47@gmail.com>
2021-11-18 17:40:12 +0300

    feat(ProjectItem): create ProjectItem component

---
+++ components/ProjectItem.tsx
@@ -0,0 +1,41 @@
+import { Flex } from "@chakra-ui/layout";
+import { Image, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+import PageAnimation from "../components/PageAnimation";
+
+interface IProps {
+  title: string;
+  cover: string;
+  id: string;
+}
+
+const ProjectItem: React.FC<IProps> = ({ title, cover, id }) => {
+  return (
+    <PageAnimation>
+      <Link href={`/projects/${id}`} passHref>
+        <Flex
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          cursor="pointer"
+          style={{ gap: 10 }}
+        >
+          <Image
+            src={cover}
+            alt={`${title}'s image`}
+            w={{ base: "300px", md: "400px" }}
+            h="250px"
+            objectFit="fill"
+            borderRadius="lg"
+            borderStyle="solid"
+            borderWidth="thin"
+          />
+          <Text fontSize="20">{title}</Text>
+        </Flex>
+      </Link>
+    </PageAnimation>
+  );
+};
+
+export default ProjectItem;
