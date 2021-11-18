491a5226f92aacfc142be318972fd78b85dd4183
GE47 <hassan2pac47@gmail.com>
2021-11-18 17:38:11 +0300

    fix: add the website assets and projects data

---
+++ projects.tsx
@@ -0,0 +1,33 @@
+const projects = [
+  {
+    title: "EduCards",
+    id: "1",
+    description:
+      "A website that helps people through their learning journey, by allowing them to create, review & share flashcards.",
+    cover: "/assets/eduCards/edu-home.png",
+    images: [
+      "/assets/eduCards/edu-main.png",
+      "/assets/eduCards/edu-new.png",
+      "/assets/eduCards/edu-review.png",
+      "/assets/eduCards/edu-details.png",
+    ],
+    demo: "https://rbc-flashcards.netlify.app/",
+    code: "https://github.com/GE47/capstone-react-flashcards",
+    techs: ["Next js", "Redux js", "Firebase", "TailwindCSS", "TipTap"],
+  },
+  {
+    title: "Portfolio",
+    id: "2",
+    description: "My personal portfolio website",
+    cover: "/assets/portfolio/portfolio-home.png",
+    images: [
+      "/assets/portfolio/portfolio-home.png",
+      "/assets/portfolio/portfolio-projects.png",
+    ],
+    demo: "",
+    code: "https://github.com/GE47/portfolio",
+    techs: ["Next js", "TypeScript", "Chakra UI", "Framer Motion"],
+  },
+];
+
+export default projects;
