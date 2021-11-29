33aacaa2bcdfdfadacac5156bcd63c3313d1c4d0
GE47 <hassan2pac47@gmail.com>
2021-11-30 01:49:08 +0300

    fix(Projects): use PageAnimation in Projects page instead of item

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -1,8 +1,7 @@
 import { Flex } from "@chakra-ui/layout";
-import { Image, Text } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 import Link from "next/link";
 
-import PageAnimation from "../components/PageAnimation";
 import AppImage from "./AppImage";
 
 interface IProps {
@@ -13,27 +12,25 @@ interface IProps {
 
 const ProjectItem: React.FC<IProps> = ({ title, cover, id }) => {
   return (
-    <PageAnimation>
-      <Link href={`/projects/${id}`} passHref>
-        <Flex
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          cursor="pointer"
-          style={{ gap: 10 }}
-        >
-          <AppImage
-            src={cover}
-            alt={`${title}'s image`}
-            w={{ base: "300px", md: "330px" }}
-            h="250px"
-            objectFit="cover"
-            borderWidth="thin"
-          />
-          <Text fontSize="20">{title}</Text>
-        </Flex>
-      </Link>
-    </PageAnimation>
+    <Link href={`/projects/${id}`} passHref>
+      <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        cursor="pointer"
+        style={{ gap: 10 }}
+      >
+        <AppImage
+          src={cover}
+          alt={`${title}'s image`}
+          w={{ base: "300px", md: "330px" }}
+          h="250px"
+          objectFit="cover"
+          borderWidth="thin"
+        />
+        <Text fontSize="20">{title}</Text>
+      </Flex>
+    </Link>
   );
 };
 
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,28 +2,31 @@ import { NextPage } from "next";
 import { Box, SimpleGrid } from "@chakra-ui/layout";
 
 import ProjectItem from "../../components/ProjectItem";
+import PageAnimation from "../../components/PageAnimation";
 import projectsData from "../../projects";
 
 const Projects: NextPage = () => {
   return (
-    <Box
-      py="10"
-      minH="80vh"
-      display="flex"
-      flexDir="column"
-      justifyContent="center"
-    >
-      <SimpleGrid columns={{ base: 1, md: 2 }} gap={5}>
-        {projectsData.map((project) => (
-          <ProjectItem
-            id={project.id}
-            key={project.id}
-            title={project.title}
-            cover={project.cover}
-          />
-        ))}
-      </SimpleGrid>
-    </Box>
+    <PageAnimation>
+      <Box
+        py="10"
+        minH="80vh"
+        display="flex"
+        flexDir="column"
+        justifyContent="center"
+      >
+        <SimpleGrid columns={{ base: 1, md: 2 }} gap={5}>
+          {projectsData.map((project) => (
+            <ProjectItem
+              id={project.id}
+              key={project.id}
+              title={project.title}
+              cover={project.cover}
+            />
+          ))}
+        </SimpleGrid>
+      </Box>
+    </PageAnimation>
   );
 };
 
